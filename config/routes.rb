6bb2ebfa92e0de93820af8c5fc6e1b841b280849
at-sacai <1216.sacai@gmail.com>
2024-10-14 21:11:40 +0900

    サインアップ、ログイン前後の処理を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   resources :tweets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
