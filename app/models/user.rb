82553afa5114e4f613b8b16575f154d5e4df26b8
at-sacai <1216.sacai@gmail.com>
2024-12-25 18:25:57 +0900

    ユーザーを検索または新規作成する処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, omniauth_providers: %i[github]
   extend Devise::Models
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      user.telephone_number = '00000000000'
+      user.date_of_birth = '0000-00-00'
+    end
+  end
 end
