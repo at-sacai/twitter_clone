d84223cbf2889d2dbee49069355dbdda62a92ff2
at-sacai <1216.sacai@gmail.com>
2024-12-26 11:05:04 +0900

    OAuth 未利用時の処理の追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -60,5 +60,16 @@ module Users
     def after_inactive_sign_up_path_for(_resource)
       tweets_path
     end
+
+    def build_resource(hash = {})
+      hash[:uid] = User.create_unique_string
+      super
+    end
+
+    def update_resource(resource, params)
+      return super if params['password'].present?
+
+      resource.update_without_password(params.except('current_password'))
+    end
   end
 end
