53eece8c192075981fc3574dc4d5515bc4219d2e
at-sacai <1216.sacai@gmail.com>
2024-12-26 16:38:17 +0900

    phone_number カラムの UNIQUE 制約の削除

--- db/migrate/20241014051538_devise_create_users.rb
+++ db/migrate/20241014051538_devise_create_users.rb
@@ -42,6 +42,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token, unique: true
     add_index :users, :unlock_token, unique: true
-    add_index :users, :phone_number, unique: true
+    add_index :users, :phone_number
   end
 end
